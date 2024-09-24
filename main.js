0ee336dcb1559f871630405f2a4f785de80c8bdd
taitnt <tantai17132002@gmail.com>
2024-09-24 10:09:44 +0700

    24/09

--- main.js
+++ main.js
@@ -47,21 +47,55 @@ const animals = [
 const getAnimals = () => animals;
 console.log("getAnimals:", getAnimals());
 
-//2. Function to filter animals by type and remove 'type' property
+// let animalTest = getAnimals();
+// animalTest[0] = null;
+// console.log(animals)
+
+// 2. Function to filter animals by type and remove 'type' property
 const getAnimal = (type) => {
+    // Check if type is not a number
+    if (typeof type !== 'number') {
+        // throw: throws the error out of the current scope and moves it to where the error was caught, If there is no try-catch, the program will stop  
+        throw new Error('Invalid type: type must be a number')
+    }
+    // Filter elements by type and remove 'type' attribute
     return animals
     .filter(animal => animal.type === type) 
     //Destructuring(separate type from object) and rest syntax (rest of the object into rest variable)
     .map(({type, ...rest}) => rest); //  Remove 'type' property
 };
-console.log("Type=0:", getAnimal(0)); //type=0
+//0(bird); 1(dog)
+try{
+    console.log("Type=Bird:", getAnimal(0)); 
+} catch (error){  
+    console.error('Error:', error.message); 
+}
+try{
+    console.log("Type=Dog:", getAnimal(1)); 
+} catch (error){
+    console.error('Error:', error.message)
+}
 
-//3. ham async lay ra name va tra ve mang string
+// 3. async function takes name and returns string array
 const getHelloName = async () => {
+
+    //a) map
     //Go through the animals array to get the names
     const Promises = animals.map(animal => hello(animal.name));
     //Use Promise.all to convert all completed promises and return the resulting array
     const results = await Promise.all(Promises);
+
+    // //b) forEach
+    // const results = []
+    // const promises = []; 
+    // animals.forEach(animal => {
+    //     const promise = hello(animal.name).then(result => { //then: wait for promise completion
+    //         results.push(result) //save results
+    //     });
+    //     promises.push(promise);
+    // });
+    // await Promise.all(promises);
+
     return results; 
 };
 (async () => {
